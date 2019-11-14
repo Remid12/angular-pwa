7c4e4b51874467ff4355909c2840108daa94790d
Rémi <remi@walkingmen.com>
2019-11-14 16:07:43 +0100

    push notif test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar: MatSnackBar) { }
+
+  pushAutorize: boolean = false;
 
   ngOnInit() {
+    Notification.requestPermission(result => {
+      if (result === 'granted') {
+        this.pushAutorize = true;
+      } else {
+        this.pushAutorize = false;
+      }
+
+      if ('Notification' in window) {
+        this.pushNotification();
+      }
+
+    });
+  }
+
+  showStatusNotif(message: string, panelClass: string) {
+    let config = new MatSnackBarConfig();
+    config.duration = 200;
+    config.panelClass = panelClass;
+
+    this.snackBar.open(message, '', config);
+  }
+
+  pushNotification() {
+    if (this.pushAutorize) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          tag: 'vibration-sample'
+        });
+      });
+    }
   }
 
 }
