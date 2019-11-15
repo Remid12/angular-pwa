e3aa7dfac2caabcdf30135c2ce46863d63f78b7f
Rémi <remi@walkingmen.com>
2019-11-15 10:46:40 +0100

    custom push notif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,14 @@ import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { OfflineComponent } from './offline/offline.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -30,6 +34,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatCardModule,
     MatToolbarModule,
     MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
@@ -10,17 +11,34 @@ export class HomeComponent implements OnInit {
 
   constructor(public snackBar: MatSnackBar) { }
 
+  public formPush = new FormGroup({
+    message: new FormControl('', [
+      Validators.required,
+    ])
+  });
+
   pushAutorize: boolean = false;
+  title: string = "Angular PWA";
+  pushOptions = {
+    body: this.formPush.value.message,
+    icon: "/assets/icons/icon-96x96.png",
+    vibrate: [200, 100, 200],
+    badge: "/assets/icons/icon-96x96.png",
+    actions: [{ action: "Detail", title: "Yolo test", icon: "/assets/icons/icon-96x96.png" }],
+    click_action: "https://material.angular.io/"
+  };
 
   ngOnInit() {
+    //Check if notifications are authorized
     Notification.requestPermission(result => {
       if (result === 'granted' && 'Notification' in window) {
         this.pushAutorize = true;
       } else {
         this.pushAutorize = false;
       }
-
     });
+
+    this.formPush.get('message').setValue('Custom message here');
   }
 
   showStatusNotif(message: string, panelClass: string) {
@@ -31,13 +49,19 @@ export class HomeComponent implements OnInit {
     this.snackBar.open(message, '', config);
   }
 
+  checkFormPush() {
+    if (this.formPush.status === 'VALID') {
+      this.pushNotification();
+    }
+  }
+
   pushNotification() {
     if (this.pushAutorize) {
+      this.pushOptions.body = this.formPush.value.message;
+
       navigator.serviceWorker.ready.then(registration => {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          tag: 'vibration-sample'
-        });
+        console.log(registration);
+        registration.showNotification(this.title, this.pushOptions);
       });
     }
   }
